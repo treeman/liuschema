160f2be4196d6affbc4d671a40ab8c15e5e5782f
Jonas Hietala <mail@jonashietala.se>
2014-11-17 15:59:58 +0100

    Move config from lib to app. Simple command line interface.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,105 @@
-#![feature(globs)]
+#![feature(slicing_syntax)]
 
 extern crate time;
 extern crate timeedit;
+extern crate getopts;
+extern crate serialize;
 
+use std::os;
+use std::io;
 use std::time::Duration;
+use getopts::{ optopt, optflag, getopts, usage, Matches };
 
-use timeedit::{ Config, DataId, TypeInfo, Course, Group };
+use timeedit::{ Course, Group };
+
+use config::Config;
+
+mod config;
 
 fn main() {
-    let conf = Config::from_file("config.json");
+    let args = os::args();
 
-    let from = time::now();
-    let to = time::at(from.to_timespec() + Duration::weeks(1));
+    let opts = [
+        optopt("c", "config", "Specify config file, default: config.json", "CFILE"),
+        optflag("h", "help", "Display this help and exit"),
+        optopt("", "search", "Search for a course/group", "TEXT"),
+        optflag("", "today", "Limit schedule to today only"),
+    ];
 
-    let s = "TATA";
-
-    let (types, typ) = timeedit::search(s, &conf);
-    let ts = match typ {
-        Course => "courses",
-        Group => "groups",
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => m,
+        Err(e) => panic!("{}", e),
     };
-    println!("Found {} {}", types.len(), ts);
+
+    let progname = args[0].clone();
+    let usage = usage("A simple cli for timeedit scheduling.", opts);
+
+    let mode = if matches.opt_present("help") {
+        Help
+    } else if matches.opt_present("search") {
+        Search
+    } else {
+        Run
+    };
+
+    let conf_file = match matches.opt_str("c") {
+        Some(c) => c,
+        None => "config.json".to_string(),
+    };
+    let conf = Config::from_file(conf_file[]);
+
+    match mode {
+        Help => help(progname[], usage[]),
+        Search => {
+            let string = matches.opt_str("search").unwrap();
+            search(string[], conf);
+        },
+        Run => run(conf, matches),
+    }
+}
+
+enum Mode {
+    Help,
+    Search,
+    Run
+}
+
+fn search(string: &str, conf: Config) {
+    println!("Searching for \"{}\"", string);
+
+    let (types, typ) = timeedit::search(string, conf.base[]);
+    let t = match typ {
+        Course => "course",
+        Group => "group",
+    };
+    println!("Found {} {}{}", types.len(), t, if types.len() == 1 { "" } else { "s" });
     for t in types.iter() {
         println!("{}", t);
     }
+}
 
-    let mut types = Vec::new();
-    //types.push(TypeInfo::new("TATA31", "", DataId::new("363733.219")));
-    types.push(TypeInfo::new("TATA49", "", DataId::new("363741.219")));
-    types.push(TypeInfo::new("FYN1", "", DataId::new("153398.205")));
+fn run(conf: Config, matches: Matches) {
+    let from = time::now();
+    let to = time::at(from.to_timespec() + Duration::weeks(1));
 
-    let entries = timeedit::schedule(types, from, to, &conf);
+    let mut entries = timeedit::schedule_from_ids(conf.data_ids, from, to, conf.base[]);
 
-    println!("Found {} entries", entries.len());
+    // Filter out day by transforming to YYYY-MM-DD and discarding non-matches
+    if matches.opt_present("today") {
+        let date_format = "%F";
+        let today = time::strftime(date_format, &from).unwrap();
+        entries = entries.into_iter().filter(|x| {
+            today == time::strftime(date_format, &x.start).unwrap()
+        }).collect();
+    }
+
     for entry in entries.iter() {
         println!("{}", entry);
     }
+}
+
+fn help(progname: &str, usage: &str) {
+    println!("Usage: {:s} [OPTION]", progname);
+    io::stdio::println(usage);
 }
 
